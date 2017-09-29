a5932c3af87a2c0877159e68a6829edbec1279af
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-09-29 18:23:47 +0200

    updater 1.54 start

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.53',
-	TWIRNew: '+Upravené menu třídění použitelných předmětů, +Přidaný jazyk:Slovenčina,',
+    TWIR: '1.54',
+    TWIRNew: '+ Kompletně předělán výběr receptů na kategorii Řemeslo, Upravení grafiky tlačítek v menu.',
 };
