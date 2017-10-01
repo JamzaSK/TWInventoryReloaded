770f22e5e8e6cdf6e2e9cfe32777c9d342383d21
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-01 11:38:57 +0200

    update 1.55

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.54',
-    TWIRNew: '+ Kompletně předělán výběr receptů na kategorii Řemeslo, Upravení grafiky tlačítek v menu.',
+    TWIR: '1.55',
+    TWIRNew: '+ Upravené rozhraní výběru použitelných předmětů, + Přidané úkolové předměty, vybavení, + Přidaná funkce otevírání několik brašen najednou, tato funkce není v této verzi aktivní, bude brzy.',
 };
