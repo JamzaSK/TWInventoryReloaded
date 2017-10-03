f42755c95eacf893daa9f86cabb4697b63cbfcce
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-03 18:13:06 +0200

    update 1.56

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.55',
-    TWIRNew: '+ Upravené uspořádání výběru použitelných předmětů (na abecedné), + Přidáno třídění pro úkolové předměty a vybavení, + Přidaná funkce otevírání několik brašen najednou, tato funkce není zatím aktivní, bude brzy.',
+    TWIR: '1.56',
+    TWIRNew: '+ Upravené menu třídění použitelných předmětů a řemesla.',
 };
