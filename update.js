fa4b9deebc1760e1b5548f812e33f33319ff8a7e
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-14 15:17:06 +0200

    update 1.61

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.60',
-    TWIRNew: '+ Přidáné ikonky pro sbírky, + Úprava kódu pro nadcházející využití.',
+    TWIR: '1.61',
+    TWIRNew: '+ Přidaná nová funkce spolu s ikonou - Denní úkoly, tahle funkce je v testovací verzi, + Bugfix.',
 };
