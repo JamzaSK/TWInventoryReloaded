ed6729c0d48b276b2d5abf5e9b39b7da8d887f89
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-17 20:02:24 +0200

    update 1.63

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.62',
-    TWIRNew: '1.62// Denní úkoly v1.0>1.1 (úprava rozhraní) //1.61 + Přidaná nová funkce spolu s ikonou - Denní úkoly, tahle funkce je v testovací verzi, + Bugfix.',
+    TWIR: '1.63',
+    TWIRNew: 'Denní úkoly v1.1>1.2 (úprava rozhraní).',
 };
