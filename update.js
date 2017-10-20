0324ad6e4300cdc7abfc8dcf5a269172409fb27a
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-20 02:05:05 +0200

    fix update 1.65

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.64',
-    TWIRNew: '+ Denní úkoly dokončeny a přejmenovány na Opakovatelné úloly, + Úprava rozhraní modulů a rozmístnení jejich ikonek.',
+    TWIR: '1.65',
+    TWIRNew: '1.65// Rework+All Fixed, 1.64// + Denní úkoly dokončeny a přejmenovány na Opakovatelné úloly, + Úprava rozhraní modulů a rozmístnení jejich ikonek.',
 };
