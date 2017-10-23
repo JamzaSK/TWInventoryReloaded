c2857b0c791afd37f359a68455330ee4e8d35c5b
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-23 15:08:24 +0200

    update 1.66

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.65',
-    TWIRNew: '1.65// Rework+All Fixed, 1.64// + Denní úkoly dokončeny a přejmenovány na Opakovatelné úloly, + Úprava rozhraní modulů a rozmístnení jejich ikonek.',
+    TWIR: '1.66',
+    TWIRNew: '+ Přidán modul (funkce) na testování - Pozice okna, + Přidáno zobrazování ID itemů v jejich popup okně, + Bugfix, - Pro chybu v kódu byl odstraněn modul (funkce) - Osobní sbírky.',
 };
