02ecb53eaaff765600b8b9b9520d3e9ab6593f3c
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-10-24 16:07:49 +0200

    X update 1.67

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.66',
-    TWIRNew: '+ Přidán modul (funkce) na testování - Pozice okna, + Přidáno zobrazování ID itemů v jejich popup okně, + Bugfix, - Pro chybu v kódu byl odstraněn modul (funkce) - Osobní sbírky.',
+    TWIR: '1.67',
+    TWIRNew: '1.67// + Přidáno tlačidlo pro resetování pozice okna, 1.66// + Přidán modul (funkce) na testování - Pozice okna, + Přidáno zobrazování ID itemů v jejich popup okně, + Bugfix, - Pro chybu v kódu byl odstraněn modul (funkce) - Osobní sbírky.',
 };
