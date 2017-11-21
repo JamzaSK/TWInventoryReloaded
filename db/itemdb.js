bd2534d1384421dfa9ab215b8db4ae3c020a4e7b
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-11-21 13:25:00 +0100

    Update itemdb.js

--- db/itemdb.js
+++ db/itemdb.js
@@ -1,4 +1,81 @@
-var db = {
-	/*Pouzitelne*/
-	Vsechny: '[1863000, 1864000, 1871000, 1872000, 1901000, 1908000, 1916000, 1938000, 1946000, 1981000, 1984000, 2285000, 2286000, 2287000, 2288000, 2289000, 2529000, 2695000, 50135000, 50136000, ]',
-};
+var DATA_Bonus = [{
+        name: TWIRlang.lang40, //výstavba
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_build.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang41, //sila úderu
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_punch.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang42, //houževnatost
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_tough.png',
+        itemsk: [1983, 2002, ]
+    }, {
+        name: TWIRlang.lang43, //vytrvalost
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_endurance.png',
+        itemsk: [1989, 2008, ]
+    }, {
+        name: TWIRlang.lang44, //zdraví
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_health.png',
+        itemsk: [1950, 1857, ]
+    }, {
+        name: TWIRlang.lang45, //jízda na koni
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_ride.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang46, //reflexy
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_reflex.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang47, //uhýbaní
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_dodge.png',
+        itemsk: [1983, 2002, ]
+    }, {
+        name: TWIRlang.lang48, //skrývaní
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_hide.png',
+        itemsk: [1989, 2008, ]
+    }, {
+        name: TWIRlang.lang49, //plavání
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_swim.png',
+        itemsk: [1950, 1857, ]
+    }, {
+        name: TWIRlang.lang50, //presnosť
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_aim.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang51, //strieľanie
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_shot.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang52, //nástraha pascí
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_pitfall.png',
+        itemsk: [1983, 2002, ]
+    }, {
+        name: TWIRlang.lang53, //manuální zručnost
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_finger_dexterity.png',
+        itemsk: [1989, 2008, ]
+    }, {
+        name: TWIRlang.lang54, //opravování
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_repair.png',
+        itemsk: [1950, 1857, ]
+    }, {
+        name: TWIRlang.lang55, //vést
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_leadership.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang56, //taktika
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_tactic.png',
+        itemsk: [2737, 1999, ]
+    }, {
+        name: TWIRlang.lang57, //obchodování
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_trade.png',
+        itemsk: [1983, 2002, ]
+    }, {
+        name: TWIRlang.lang58, //zachádzení s koňmi
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_animal.png',
+        itemsk: [1989, 2008, ]
+    }, {
+        name: TWIRlang.lang59, //vystupko
+        img: 'https://jamzask.github.io/TWInventoryReloaded/ikony/schopnosti/skillicon_appearance.png',
+        itemsk: [1950, 1857, ]
+    }, ];
