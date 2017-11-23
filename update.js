5a05bbb4d929c59ee200d123c57ac076befcb7fe
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-11-23 01:12:32 +0100

    update 1.80

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
     TWIR: '1.80',
-    TWIRNew: 'Faster, Better, Clearer, + Třídění podle schopností (pušky WIP), + Třídění nyní podporuje vylepšené předměty, + Přidána možnost finanční podpory vývoje scriptu a mnoho dalšího!',
+    TWIRNew: 'Faster, Better, Clearer > + Třídění podle schopností (pušky WIP), + Třídění nyní podporuje vylepšené předměty, + Přidána možnost finanční podpory vývoje scriptu a mnoho dalšího!',
 };
