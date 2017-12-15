2d119493b2d8188c49f94fe80669e124ec308a83
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2017-12-15 10:58:55 +0100

    update 1.85

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.84',
-    TWIRNew: '+ Ve funkci změna stylu inventáře lze nyní přepínat mezi klasickým a event stylem. Veselé Vánoce!',
+    TWIR: '1.85',
+    TWIRNew: 'Up databázy.',
 };
