be43f8a04415a998f799c696b5aa5de0b6f776fe
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-05-08 14:49:23 +0200

    up 1.92

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
     TWIR: '1.92',
-    TWIRNew: '+ Script bol updatovaný na verziu hry 2.76, addon "Zdieľanie" bol upravený. - Z triedenia "Použiteľné" boli odobraté "Poklady", "Úlohové predmety" a "Vybavenie", vrátia sa v novej kategórii v ďalšom update. (EN: + Script has been updated to game version 2.76, addon "Sharing" has been modified. - "Treasures", "Quest items" and "Equipment" have been removed from the "Usable" category, they will be returned to the new category in the next update.)',
+    TWIRNew: '+ Script bol updatovaný na verziu hry 2.76, addon "Zdieľanie" bol upravený. - Z triedenia "Použiteľné" boli odobraté "Poklady", "Úlohové predmety" a "Vybavenie", vrátia sa v novej kategórii v ďalšom update. (EN: + Script has been updated to game version 2.76, addon "Sharing" has been modified. - "Treasures", "Quest items" and "Equipment" have been removed from the "Usable" category, they will be returned to new category in the next update.)',
 };
