989a62feb25b9672ba9496adb997726361e57a07
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-06-01 21:27:56 +0200

    up 1.93 part 2

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '1.93',
-    TWIRNew: '+ Script bol updatovaný na verziu hry 2.78, Upravené zobrazovanie kategórii, Bugfix',
+    TWIR: '1.931',
+    TWIRNew: '+ Script bol updatovaný na verziu hry 2.78, Upravené zobrazovanie kategórii, Bugfix //1.931',
 };
