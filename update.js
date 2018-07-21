d33a93c6b196f9fb493e20eb90d506a26d57445e
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-07-21 13:38:14 +0200

    2.02

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '2.01',
-    TWIRNew: 'Update 2.01 (pre launch) - Oprava chyby pri updatovaní z 1.931, Script teraz podporuje prehliadač Opera',
+    TWIR: '2.02',
+    TWIRNew: 'Update 2.02 (pre launch) - Oprava chyby pri updatovaní z 1.931, Script teraz podporuje prehliadač Opera',
 };
