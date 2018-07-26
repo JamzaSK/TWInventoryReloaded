deccc8527050894cd87c36436cc8607f50afdfce
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-07-26 21:06:50 +0200

    up

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
     TWIR: '2.06',
-    TWIRNew: 'TWIR 2 (pre launch) Update 2.06: Update databázy, Bugfix.',
+    TWIRNew: 'TWIR 2 (pre launch) Update 2.06: Update databázy, Bugfix',
 };
