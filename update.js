821e880eaafa2fef2c1156019f6f254a63cf4628
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-08-25 01:52:08 +0200

    2.121

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '2.12',
-    TWIRNew: 'TWIR 2 (pre launch) Update 2.12: Code changes',
+    TWIR: '2.121',
+    TWIRNew: 'TWIR 2 (pre launch) Update 2.121: Code changes',
 };
