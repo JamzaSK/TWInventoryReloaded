e834a51de40fc9f5bc8780fe58ccd890a444200f
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-09-13 22:07:41 +0200

    2.124

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 var scriptUpdate={
-    TWIR: '2.123',
-    TWIRNew: 'TWIR 2 (pre launch) Update 2.123: Updated to Game version 2.84',
+    TWIR: '2.124',
+    TWIRNew: 'TWIR 2 (pre launch) Update 2.124: Updated to Game version 2.85 + Design and code changes',
 };
