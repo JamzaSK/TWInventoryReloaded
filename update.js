bcd12d4fb634408c5abfd30d9dc772ac62b84db0
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-11-25 22:28:00 +0100

    2.132

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.131',
+    TWIR: '2.132',
     TWIRrev: '3',
-    TWIRNew: 'Fully reworked.',
+    TWIRNew: 'New bonus search feature, reworked filter and sort feature (please make sure this version fully replaced the old one or remove old version manually)',
 };
 
