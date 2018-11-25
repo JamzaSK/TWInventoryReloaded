13b9f695188a366e6436dd83f958539075a7a569
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2018-11-26 00:29:14 +0100

    fix

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.132',
-    TWIRrev: '3',
-    TWIRNew: 'New bonus search feature, reworked filter and sort feature (please make sure this version fully replaced the old one or remove old version manually)',
+    TWIRrev: '4',
+    TWIRNew: 'Fix to previous update. Please remove older version manually in Tampermonkey Dashboard! Previous update: New options window, added bonus search, reworked filter and sort feature.',
 };
 
