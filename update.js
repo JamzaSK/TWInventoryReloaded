0723a3fc1cef7ba9bc8e0ea688be0d263facd3b2
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-01-20 23:43:05 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.140',
-    TWIRrev: '4',
+    TWIRrev: '5',
     TWIRNew: 'New icons, updated menus, updated item set list, updated filter, sort and bonus search, better popups with additional informations including crafting recipes, job names and much more!',
 };
 
