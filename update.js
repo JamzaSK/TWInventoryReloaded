fe9c6a2678f77a62d661ce618f47df9f551b98ea
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-01-20 22:01:06 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.133',
+    TWIR: '2.140',
     TWIRrev: '4',
-    TWIRNew: 'Added 2.92 items, fixed compatibility issue, fixed sorting for weapon damage with updated sorting menu. Merry Christmas!',
+    TWIRNew: 'New icons, updated menus, updated item set list, updated filter, sort and bonus search, better popups with additional informations including crafting recipes, job names and much more!',
 };
 
