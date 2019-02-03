a58431016de25ed03119b2cf0927a690e6a322c0
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-02-03 21:16:19 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.141',
+    TWIR: '2.142',
     TWIRrev: '5',
-    TWIRNew: 'Added new function - Market Watcher - simple function that scans market and tracks items and their prices. Fixed for latest TW LeoTools.',
+    TWIRNew: 'New "Partial Bonus" - comparing bonuses based on number of items equipped from another set, updated Market Watcher - now It will scan market pages instead of items by ID, updated Spanish language, bugfix. IMPORTANT NOTE: Please make sure this update fully rewrite your older version. If not, please remove older (duplicate) version manually!',
 };
 
