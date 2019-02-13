1f65546df19f842ac83735ac0ed178f0ff778ea6
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-02-13 21:46:28 +0100

    2.143

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.142',
+    TWIR: '2.143',
     TWIRrev: '5',
-    TWIRNew: 'New "Partial Bonus" - comparing bonuses based on number of items equipped from another set, updated Market Watcher - now It will scan market pages instead of items by ID, updated Spanish language, bugfix. IMPORTANT NOTE: Please make sure this update fully rewrite your older version. If not, please remove older (duplicate) version manually!',
+    TWIRNew: 'New Market Alert feature - now you can set items what you want and It will automatically alert you when these items will be seen on the market. Updated item popups - added item counts. Updated to game version 1.94.',
 };
 
