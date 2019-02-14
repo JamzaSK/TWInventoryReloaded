06f4603e8d739d89506598405f92e459c848cb87
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-02-14 22:21:38 +0100

    bugfix rev6

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.143',
-    TWIRrev: '5',
-    TWIRNew: 'New Market Alert feature - now you can set items what you want and It will automatically alert you when these items will be seen on the market. Updated item popups - added item counts. Updated to game version 2.94.',
+    TWIRrev: '6',
+    TWIRNew: 'Bugfix, Added sound to Market Alert - yes, you can mute it :P',
 };
 
