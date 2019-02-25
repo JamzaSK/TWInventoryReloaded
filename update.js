32087779f35ccee91da63447e2a853d1dfd18ae0
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-02-25 20:53:35 +0100

    2.144

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.143',
+    TWIR: '2.144',
     TWIRrev: '6',
-    TWIRNew: 'Bugfix, Added sound to Market Alert - yes, you can mute it :P',
+    TWIRNew: 'Better assignment for items in all categories. Added new search keywords. Code improvements and many more!',
 };
 
