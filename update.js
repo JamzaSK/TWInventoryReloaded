1bbb1b5e591e7f7ee9e1a5c4c51555b84e5df364
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-02-25 21:43:55 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.144',
-    TWIRrev: '6',
+    TWIRrev: '7',
     TWIRNew: 'Better assignment for items in all categories. Added new search keywords. Code improvements and many more!',
 };
 
