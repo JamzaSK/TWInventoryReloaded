1e189a6381cc5f85e474a9c8c472a7f3810f8063
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-03-04 20:35:57 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.145',
-    TWIRrev: '8',
-    TWIRNew: 'Added Italian translation, Speed and Motivation category is now splitted to Work/Duel motivation and Speed/Waytime shortening, Fixed: Instant Wear button, Filter, ScrollPane bug',
+    TWIRrev: '9',
+    TWIRNew: 'AdBlock problem fixed.',
 };
 
