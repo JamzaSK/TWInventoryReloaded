2ad6359ee9aa8141b9a62bc5a1eff82bfeced8aa
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-03-05 23:58:03 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.146',
-    TWIRrev: '09',
-    TWIRNew: 'AdBlock problem fixed.',
+    TWIRrev: '10',
+    TWIRNew: 'Added trader name and level for base items. Very useful for Smoking guns ;-) Disabled by default. If you want to, you can meet me there @Jamza.',
 };
 
