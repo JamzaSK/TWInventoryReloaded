b0fe6e8135371861477fb4d266bbecf428b1f5be
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-03-10 22:04:55 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.147',
     TWIRrev: '10',
-    TWIRNew: 'Improved job popup (added xp/15s and drop chance), Added Avatar part category to Useable, Improved loading., ',
+    TWIRNew: 'Improved job popup (added xp/15s and drop chance), Added Avatar part category to Useable, Improved loading.',
 };
 
