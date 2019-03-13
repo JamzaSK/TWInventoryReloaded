6d78904b7ce482fba713c6b5b2c820525b97418b
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-03-13 21:12:47 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
     TWIR: '2.147',
-    TWIRrev: '10',
-    TWIRNew: 'Improved job popup (added xp/15s and drop chance), Added Avatar part category to Useable, Improved loading.',
+    TWIRrev: '11',
+    TWIRNew: 'Firefox bug (job popup level <20) fixed.',
 };
 
