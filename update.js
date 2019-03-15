f3b7eb98ea156edeb569927ed100d5e1ab1cb9ec
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-03-16 00:46:45 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 var scriptUpdate={
-    TWIR: '2.147',
+    TWIR: '2.148',
     TWIRrev: '12',
     TWIRNew: 'Popup bug for non Toolkit users should be fixed now. Let me know please. Improved popups and discount price for shop items is now correct.',
 };
