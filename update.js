d205926e2167292b41cc967e6225cb4b34b4b9f6
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-04-15 21:46:13 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.150',
+    TWIR: '2.151',
     TWIRrev: '12',
-    TWIRNew: 'Additional informations in player profiles.',
+    TWIRNew: 'Fort battle update! Added online status and ranks for both sides (based on chat), added chat topic to battle preparation window, added button for fast travel to fort.',
 };
 
