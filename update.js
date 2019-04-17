89b824cc59dfaef720215faae9ae112dda840411
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-04-17 21:17:24 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.151',
+    TWIR: '2.152',
     TWIRrev: '12',
-    TWIRNew: 'Fort battle update! Added online status and ranks for both sides (based on chat), added chat topic to battle preparation window, added button for fast travel to fort. Some changes to market price, now you can watch prices for all items in the game.',
+    TWIRNew: 'MPI error fixed. Sorry about that.',
 };
 
