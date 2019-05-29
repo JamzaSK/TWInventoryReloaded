222ae482765e9dd426da88a96fc594acc97c90d9
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-05-29 09:38:30 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.155',
+    TWIR: '2.156',
     TWIRrev: '12',
-    TWIRNew: 'Set menu order is now based on search value, fixed text overflow in job window (TW bug)'
+    TWIRNew: 'PL translation and problem with Partial bonus fixed.'
 };
 
