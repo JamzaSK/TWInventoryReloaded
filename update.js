18cc50bf553df84cd8e0efe0f64a392b7fc47a6e
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-06-09 20:29:28 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 var scriptUpdate={
-    TWIR: '2.156',
+    TWIR: '2.157',
     TWIRrev: '12',
     TWIRNew: 'Market Watcher changes: new design and order, added option to scan trader items, option to scan items needed for collections, option to watch full set, added Import and Export for watched items, option to automatically delete old scan data and other improvements.'
 };
