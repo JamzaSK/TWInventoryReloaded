865e25a809057499dbf5fe9a7e11c086736c82bf
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-06-11 21:54:27 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 var scriptUpdate={
-    TWIR: '2.158',
+    TWIR: '2.159',
     TWIRrev: '12',
     TWIRNew: '2.158: Fixed collections in MW, added battle count. 2.157: Market Watcher changes: new design and order, added option to scan trader items, option to scan items needed for collections, option to watch full set, added Import and Export for watched items, option to automatically delete old scan data and other improvements.'
 };
