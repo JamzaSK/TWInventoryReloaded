0384ca79d15fe90d02bb9ea17827d558d4618105
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-08-06 12:18:02 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.159',
+    TWIR: '2.160',
     TWIRrev: '12',
-    TWIRNew: '2.158: Fixed collections in MW, added battle count. 2.157: Market Watcher changes: new design and order, added option to scan trader items, option to scan items needed for collections, option to watch full set, added Import and Export for watched items, option to automatically delete old scan data and other improvements.'
+    TWIRNew: 'New functions: Smarter chat (video and image URL recognition), message menu > you can send a telegram to other town/alliance members or players joined in fort battle, new inventory stats based on the search value, better alliance profile with join date, better player popup in fort battles. Added Hungarian language, thanks to Aviendha.'
 };
 
