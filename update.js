1782f535b72f7cdca241a5f107fcb24f06c38876
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-08-10 12:04:22 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.160',
+    TWIR: '2.161',
     TWIRrev: '12',
-    TWIRNew: 'New functions: Smarter chat (video and image URL recognition), message menu > you can send a telegram to other town/alliance members or players joined in fort battle, new inventory stats based on the search value, better alliance profile with join date, better player popup in fort battles. Added Hungarian language, thanks to Aviendha.'
+    TWIRNew: 'Attackers / Defenders option in the message menu (recruitment window) moved to cemetery window, added Not at fort and Absent players option instead'
 };
 
