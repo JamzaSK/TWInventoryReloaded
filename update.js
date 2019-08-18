236720f9476c084aa5c83ab00b27d4eec40850df
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-08-18 21:24:27 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 var scriptUpdate={
     TWIR: '2.165',
     TWIRrev: '12',
-    TWIRNew: 'Compatibility with TW Kappa - Chat Enhancements.'
+    TWIRNew: 'Compatibility with TW Kappa, better tooltip for item set menu.'
 };
