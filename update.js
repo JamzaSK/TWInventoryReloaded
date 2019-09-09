bfc15ad68a8a7f4d10540d6c9f2133665bef094b
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2019-09-09 09:46:16 +0200

    Update update.js

--- update.js
+++ update.js
@@ -1,5 +1,5 @@
 var scriptUpdate={
     TWIR: '2.168',
     TWIRrev: '12',
-    TWIRNew: 'Russian translation - thanks to Luntik, added Custom sets, option to change the number of rows in the selection menu, option to always display bonuses (per level) on your level'
+    TWIRNew: 'Russian translation - thanks to Luntik, added Custom sets, option to change the number of rows for the selection menu in your inventory, option to always display bonuses (per level) on your level'
 };
