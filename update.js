0a1d053e0abb3ceae0ab239f0eb0750bdd3e30b2
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2020-02-11 21:17:37 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,6 +1,6 @@
 var scriptUpdate={
-    TWIR: '2.182',
-    TWIRrev: '12',
-    TWIRNew: '',
-    changelog: true
+    TWIR: '2.183',
+    TWIRrev: '13',
+    TWIRNew: 'Small bug fix for reminder alerts.',
+    changelog: false
 };
