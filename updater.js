4edbaed87eeda296d9fb44872449a4455433a116
JamzaSK <32229252+JamzaSK@users.noreply.github.com>
2020-05-20 21:46:42 +0200

    Update updater.js

--- updater.js
+++ updater.js
@@ -1,6 +1,6 @@
 TWIR_update = {
 	version: '2.195',
-	revision: '17',
-	changes: '- you can mark and track the player and his position in the fort battle via Tracker',
+	revision: '18',
+	changes: '- Bugfix',
 	changelog: false
 };	
